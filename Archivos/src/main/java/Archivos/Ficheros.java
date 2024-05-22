5c15e9e9b1df21469c082d0afb584544dcdb240d
110528 <Valdez.et32@gmail.com>
2024-05-22 12:38:28 -0300

    clase 22

--- Archivos/src/main/java/Archivos/Ficheros.java
+++ Archivos/src/main/java/Archivos/Ficheros.java
@@ -3,39 +3,81 @@ package Archivos;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 
 public class Ficheros {
-	
+
 	private File archivo;
 	private PrintStream ps;
-	
-	public Ficheros()
-	{
-		String ruta = "";  //   " c:\\User\\ "
+
+	public Ficheros() {
+		String ruta = ""; // " c:\\User\\ "
 		String nombre = "mario";
 		String extension = ".txt";
-		
-		archivo =  new File( ruta.concat(nombre.concat(extension))  );
+
+		archivo = new File(ruta.concat(nombre.concat(extension)));
 	}
-	
-	public void createFilePrintStream(File a)
-	{
+
+	public File getArchivo() {
+		return this.archivo;
+	}
+
+	public void createFilePrintStream(File a) {
 		FileOutputStream fos = null;
-		
+
 		try {
 			fos = new FileOutputStream(a);
-			ps = new PrintStream(fos , true); //mode append true = no sobreescribe
-			
+			ps = new PrintStream(fos, true); // mode append true = no sobreescribe
+
 			ps.println("holaa mundo");
 			ps.println("Chauu mundo");
-			
+
 			ps.flush();
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		} finally {
+			if (fos != null)
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 		}
-		
+
 	}
-	
+
+	public void createFilePrinter(File a) {
+		FileWriter fw = null;
+		PrintWriter pw = null;
+
+		try {
+
+			if (!a.exists()) {
+				a.createNewFile();
+			}
+			fw = new FileWriter(a, true);
+			pw = new PrintWriter(fw);
+
+			pw.println("OTRO MUNDO");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+
+			if (pw != null)
+				pw.close();
+
+			if (fw != null)
+				pw.close();
+		}
+	}
+
 }
--- Archivos/src/main/java/Archivos/main.java
+++ Archivos/src/main/java/Archivos/main.java
@@ -3,8 +3,11 @@ package Archivos;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Ficheros archivo = new Ficheros();
+		
+		//archivo.createFilePrintStream(archivo.getArchivo());
+	archivo.createFilePrinter(archivo.getArchivo());
 	}
 
 }
