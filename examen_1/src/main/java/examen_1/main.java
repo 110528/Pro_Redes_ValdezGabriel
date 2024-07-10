c2522525acec24d8b5a37c3b49fd3fe531759857
110528 <Valdez.et32@gmail.com>
2024-07-10 13:36:32 -0300

    comit por las dudas

---
+++ examen_1/src/main/java/examen_1/main.java
@@ -0,0 +1,15 @@
+package examen_1;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Evaluacion ejercicios = new Evaluacion();
+		
+		ejercicios.mostrarMenu();
+		
+	}
+
+}
