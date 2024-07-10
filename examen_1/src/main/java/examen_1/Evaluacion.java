9d2d1dc57ab5d950595956133509389605408421
110528 <Valdez.et32@gmail.com>
2024-07-10 15:08:09 -0300

    Examen final terminado

--- examen_1/src/main/java/examen_1/Evaluacion.java
+++ examen_1/src/main/java/examen_1/Evaluacion.java
@@ -9,57 +9,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.lang.System.Logger;
-import java.lang.System.Logger.Level;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Evaluacion {
 
-	private static PrintStream ps = System.out;
+
+	static PrintStream ps = new PrintStream(System.out);
+
 	public static File original = new File("datos.dat");
-	public static File copia = new File(" tuti_fruti.txt");
-
-	public static void editarArchivo() {
-
-		FileReader Fr = null;
-		BufferedReader Br = null;
-		FileWriter Fw = null;
-		PrintWriter Pw = null;
-
-		try {
-			Fr = new FileReader(original);
-			Br = new BufferedReader(Fr);
-
-			if (!copia.exists()) {
-
-				Fw = new FileWriter(copia);
-				Pw = new PrintWriter(Fw);
-
-				String renglon;
-				while ((renglon = Br.readLine()) != null) {
-
-					String datos = renglon.substring(1, 2).toUpperCase() + renglon.replace(".", ";");
-
-					Pw.println(datos);
-					Pw.flush();
-				}
-				Pw.close();
-				Fw.close();
-			}
-			Br.close();
-			Fr.close();
-
-			if (original.exists())
-				original.delete();
-
-		} catch (FileNotFoundException e) {
-			Logger.getLogger(Evaluacion.class.getName()).log(Level.WARNING, null, e);
-		} catch (IOException e) {
-			Logger.getLogger(Evaluacion.class.getName()).log(Level.WARNING, null, e);
-		} finally {
-
-		}
-	}
-	public void mostrarMenu() {
+	public static File copia = new File("tuti_fruti.csv");
+	
+	public static void mostrarMenu() {
 		while (true) {
 			ps.println("Menú");
 			ps.println();
@@ -108,46 +69,33 @@ public class Evaluacion {
 		}
 	}
 
-	
-	public String entradaDeDatos() {
-		String cadena = "";
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			cadena = br.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return cadena;
-	}
-
-	File archivo = new File("datos.dat");
-
-	public void mostrarTuti() {
+	public static void mostrarTuti() {
 
 		ps.println(Utils.ANSI_CYAN + Utils.ANSI_BLACK_BOLD + "Tutti-Fruti" + Utils.ANSI_RESET);
 		ps.println();
 
 		try {
 
-			FileReader fr = new FileReader("datos.dat");
+			FileReader fr = new FileReader("tuti_fruti.csv");
 
 			BufferedReader br = new BufferedReader(fr);
 
 			String linea;
 
-			ps.println(Utils.ANSI_BLUE + "COLOR\t" + "ANIMAL\t" + "COSA\t" + "COMIDA\t" + Utils.ANSI_RESET);
+			ps.println(Utils.ANSI_BLUE + "LETRA\t" + "COLOR\t\t" + "ANIMAL\t" + "COSA\t" + "COMIDA\t" + Utils.ANSI_RESET);
 
 			while ((linea = br.readLine()) != null) {
 
-				String[] datos = linea.split("\\.");
+				String[] datos = linea.split("\\;");
 
 				String Letra = datos[0];
 				String Animal = datos[1];
 				String Color = datos[2];
 				String Cosa = datos[3];
 				String Comida = datos[4];
-
-				ps.print(Utils.ANSI_GREEN + Animal + "\t" + Utils.ANSI_RESET);
+				
+				ps.print(Utils.ANSI_YELLOW + Letra + "\t" + Utils.ANSI_RESET); 
+				ps.print(Utils.ANSI_GREEN + Animal + "\t\t" + Utils.ANSI_RESET);
 				ps.print(Utils.ANSI_RED + Color + "\t" + Utils.ANSI_RESET);
 				ps.print(Utils.ANSI_PURPLE + Cosa + "\t" + Utils.ANSI_RESET);
 				ps.print(Utils.ANSI_CYAN + Comida + "\t" + Utils.ANSI_RESET);
@@ -162,4 +110,81 @@ public class Evaluacion {
 		}
 	}
 
+	
+	public static  String entradaDeDatos() {
+		String cadena = "";
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			cadena = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return cadena;
+	}
+
+
+
+
+	public static void editarArchivo() {
+
+		FileReader Fr = null;
+		BufferedReader Br = null;
+		FileWriter Fw = null;
+		PrintWriter Pw = null;
+
+		try {
+			Fr = new FileReader(original);
+			Br = new BufferedReader(Fr);
+
+			if (!copia.exists()) {
+
+				Fw = new FileWriter(copia);
+				Pw = new PrintWriter(Fw);
+
+				String renglon;
+				while ((renglon = Br.readLine()) != null) {
+
+					String datos = renglon.substring(1, 2).toUpperCase() + renglon.replace(".", ";");
+
+					Pw.println(datos);
+					Pw.flush();
+				}
+				Pw.close();
+				Fw.close();
+			}
+			Br.close();
+			Fr.close();
+
+			if (original.exists())
+				original.delete();
+
+		} catch (FileNotFoundException e) {
+			Logger.getLogger(Evaluacion.class.getName()).log(Level.WARNING, null, e);
+		} catch (IOException e) {
+			Logger.getLogger(Evaluacion.class.getName()).log(Level.WARNING, null, e);
+		} finally {
+
+		}
+	}
+	
+
+	static void archivoexiste() {
+
+		if (!original.exists()) {
+			if( copia.exists() )
+				mostrarMenu();
+
+		} else if (original.exists()) {
+			editarArchivo();
+			mostrarMenu();
+		}
+	
+}
+
+		
+
+	
+	
+
+	
 }
--- examen_1/src/main/java/examen_1/main.java
+++ examen_1/src/main/java/examen_1/main.java
@@ -1,15 +1,10 @@
 package examen_1;
 
-
-
 public class main {
 
 	public static void main(String[] args) {
 		
-		Evaluacion ejercicios = new Evaluacion();
-		
-		ejercicios.mostrarMenu();
-		
+		Evaluacion.archivoexiste();
 	}
 
 }
