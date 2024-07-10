3abf787e51835fa346834dadac4e2d828d64c011
110528 <Valdez.et32@gmail.com>
2024-07-10 13:16:32 -0300

    Ecamen mio

---
+++ examen_1/src/main/java/examen_1/main.java
@@ -0,0 +1,15 @@
+package examen_1;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Ejercicio ejercicios = new Ejercicio();
+		
+		ejercicios.mostrarMenu();
+		
+	}
+
+}
