4f18fa4f56938a6fc92c264409249ca7e84882bf
110528 <Valdez.et32@gmail.com>
2024-11-20 10:07:11 -0300

    TP_Final_Sockets

---
+++ TP_Final/src/main/java/TP_Final_Sockets/mainCliente.java
@@ -0,0 +1,10 @@
+package TP_Final_Sockets;
+
+public class mainCliente {
+
+	public static void main(String[] args) {
+		Cliente cli = new Cliente();
+
+	}
+
+}
---
+++ TP_Final/src/main/java/TP_Final_Sockets/mainServidor.java
@@ -0,0 +1,12 @@
+package TP_Final_Sockets;
+
+public class mainServidor {
+
+	public static void main(String[] args) {
+		
+		Servidor server = new Servidor();
+		server.start();
+
+	}
+
+}
